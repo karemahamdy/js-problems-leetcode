73e5990538e7f6beb16d579f2e716c155f030e5d
Karema <karemahamdy51@gmail.com>
2024-07-11 03:25:44 +0300

    1550. Three Consecutive Odds

--- index.js
+++ index.js
@@ -154,3 +154,15 @@ return arr
 nums = [1,2,1]
 
 console.log(getConcatenation(nums))
+
+
+//1550. Three Consecutive Odds
+var threeConsecutiveOdds = function(arr) {
+  let count = 0;
+  for(let i=0; i<arr.length; i++) {
+      if(arr[i]%2 == 1) count++;
+      else count = 0;
+      if(count >= 3) return true; 
+  }
+  return false;
+};
