c56c608aa53a8e52453effc22986259dfe01516a
Karema <karemahamdy51@gmail.com>
2024-07-21 15:23:01 +0300

    1431. Kids With the Greatest Number of Candies

--- index.js
+++ index.js
@@ -286,3 +286,18 @@ var numberOfEmployeesWhoMetTarget = function(hours, target) {
   hours.forEach(e => (e >= target ? count++ : count));
   return count;
 };
+
+// 1431. Kids With the Greatest Number of Candies
+var kidsWithCandies = function(candies, extraCandies) {
+  let result = []
+    const maxCandies = Math.max(...candies);
+  for(let i=0; i<candies.length; i++ ){
+      if (candies[i] + extraCandies >= maxCandies ) {
+          result.push(true)
+      } else {
+          result.push(false)
+  }
+      }
+
+  return result
+};
