30488d23d1dee0247894e906391b8068b3b141da
Karema <karemahamdy51@gmail.com>
2024-07-21 14:44:51 +0300

    2373. Largest Local Values in a Matrix

--- index.js
+++ index.js
@@ -247,3 +247,35 @@ var numJewelsInStones = function(jewels, stones) {
  var interpret = function(command) {
   return command.split("()").join("o").split("(al)").join("al");
 };
+
+
+// 2373. Largest Local Values in a Matrix
+
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var largestLocal = function (grid) {
+  const nRows = grid.length;
+  const nCols = grid[0].length;
+
+  let result = new Array(nRows - 2).fill().map(() => new Array(nCols - 2).fill(0));
+
+  for (let row = 0; row < nRows - 2; row++) {
+      for (let col = 0; col < nCols - 2; col++) {
+          result[row][col] = findLargest(grid, row, col);
+      }
+  }
+
+  return result;
+};
+
+function findLargest(grid, row, col) {
+  let best = grid[row][col];
+  for (let i = row; i < row + 3; i++) {
+      for (let j = col; j < col + 3; j++) {
+          best = Math.max(best, grid[i][j]);
+      }
+  }
+  return best;
+}
