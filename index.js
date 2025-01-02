607fa836205fd6611cb076eae9ecff39068d4313
Karema <karemahamdy51@gmail.com>
2025-01-02 22:32:51 +0200

    feat: add 2630. Memoize II

--- index.js
+++ index.js
@@ -392,3 +392,83 @@ var constructRectangle = function(area) {
    width -- 
    return [area / width , width ] 
 };
+
+
+// 2630. Memoize II
+// Given a function fn, return a memoized version of that function.
+// A memoized function is a function that will never be called twice with the same inputs.Instead it will return a cached value.
+// fn can be any function and there are no constraints on what type of values it accepts.Inputs are considered identical if they are === to each other.
+//   Example 1:
+// Input:
+// getInputs = () => [[2, 2], [2, 2], [1, 2]]
+// fn = function (a, b) { return a + b; }
+// Output: [{ "val": 4, "calls": 1 }, { "val": 4, "calls": 1 }, { "val": 3, "calls": 2 }]
+// Explanation:
+// const inputs = getInputs();
+// const memoized = memoize(fn);
+// for (const arr of inputs) {
+//   memoized(...arr);
+// }
+// For the inputs of(2, 2): 2 + 2 = 4, and it required a call to fn().
+// For the inputs of(2, 2): 2 + 2 = 4, but those inputs were seen before so no call to fn() was required.
+// For the inputs of(1, 2): 1 + 2 = 3, and it required another call to fn() for a total of 2.
+
+class LookupTree {
+  map = new Map();
+
+  hasValue = false;
+
+  value = null;
+
+  getValueHelper(path, i) {
+    const key = path[i];
+    if (i >= path.length) {
+      if (this.hasValue) {
+        return { value: this.value, success: true };
+      } else {
+        return { value: undefined, success: false };
+      }
+    } else {
+      if (this.map.has(key)) {
+        return this.map.get(key).getValueHelper(path, i + 1);
+      } else {
+        return { value: undefined, success: false };
+      }
+    }
+  }
+
+  getValue(path) {
+    return this.getValueHelper(path, 0);
+  }
+
+  setValueHelper(path, i, value) {
+    const key = path[i];
+    if (i >= path.length) {
+      this.value = value;
+      this.hasValue = true;
+    } else {
+      if (!this.map.has(key)) {
+        this.map.set(key, new LookupTree());
+      }
+      return this.map.get(key).setValueHelper(path, i + 1, value);
+    }
+  }
+
+  setValue(path, value) {
+    return this.setValueHelper(path, 0, value);
+  }
+}
+
+function memoize(func) {
+  const tree = new LookupTree();
+  const newFunction = (...params) => {
+    const cache = tree.getValue(params);
+    if (cache.success) {
+      return cache.value;
+    }
+    const result = func(...params);
+    tree.setValue(params, result);
+    return result;
+  };
+  return newFunction;
+}
