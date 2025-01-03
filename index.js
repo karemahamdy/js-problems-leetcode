3d08fb9a5cba6a3dff00aad09310d5c64b5b9bda
Karema <karemahamdy51@gmail.com>
2025-01-03 22:50:19 +0200

    isAnagram

--- index.js
+++ index.js
@@ -489,3 +489,17 @@ var checkIfInstanceOf = function (obj, classFunction) {
   return false;
 };
 
+// isAnagram
+var isAnagram = function (s, t) {
+  s = s.split("").sort()
+  t = t.split("").sort()
+
+  if (s.length !== t.length)
+    return false;
+
+  for (var i = 0; i < s.length; i++)
+    if (s[i] !== t[i])
+      return false;
+
+  return true;
+};
