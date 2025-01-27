54e6dcefbe365c7ee51b6434781e4f7df2c373b2
karemahamdy <karemahamdy51@gmail.com>
2025-01-27 10:55:47 +0200

    feat :add new problem

--- index.js
+++ index.js
@@ -503,4 +503,15 @@ var isAnagram = function (s, t) {
   return true;
 };
 
-// 
+// GCD problem 
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+let gcdOfStrings = function(str1, str2) {
+  if ( str1 + str2 !== str2 + str1) return "" ;
+   function gcd(a, b) {
+     return b === 0 ? a : gcd(b, a % b);
+ }
+ let gcdLength =    gcd(str1.length, str2.length);
+return str1.substring(0, gcdLength);
+};
