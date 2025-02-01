45a637e481ac76875fbe0f74cad82042af60310c
Karema <karemahamdy51@gmail.com>
2025-02-01 22:34:53 +0200

    ..

--- index.js
+++ index.js
@@ -503,4 +503,24 @@ var isAnagram = function (s, t) {
   return true;
 };
 
-// 
+// Delete the Middle Node of a Linked List
+
+var deleteMiddle = function (head) {
+  if (!head || !head.next) return null;
+
+  let slow = head;
+  let fast = head;
+  let prev = null;
+
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  if (prev) {
+    prev.next = slow.next;
+  }
+
+  return head;
+};
