62949304c41f06022eba3b87447a95211a251746
Karema <karemahamdy51@gmail.com>
2025-02-06 21:09:23 +0200

    feat :  1726. Tuple with Same Product

--- index.js
+++ index.js
@@ -566,3 +566,35 @@ var oddEvenList = function (head) {
 
   return head;
 };
+
+// 1726. Tuple with Same Product
+// Given an array nums of distinct positive integers, return the number of tuples(a, b, c, d) such that a * b = c * d where a, b, c, and d are elements of nums, and a != b != c != d.
+  // Example 1:
+// Input: nums = [2, 3, 4, 6]
+// Output: 8
+// Explanation: There are 8 valid tuples:
+// (2, 6, 3, 4), (2, 6, 4, 3), (6, 2, 3, 4), (6, 2, 4, 3)
+// (3, 4, 2, 6), (4, 3, 2, 6), (3, 4, 6, 2), (4, 3, 6, 2)
+  
+function tupleSameProduct(nums) {
+  let productMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      let product = nums[i] * nums[j];
+      if (productMap.has(product)) {
+        productMap.set(product, productMap.get(product) + 1);
+      } else {
+        productMap.set(product, 1);
+      }
+    }
+  }
+
+  let result = 0;
+  for (let count of productMap.values()) {
+    if (count > 1) {
+
+      result += (count * (count - 1) / 2) * 8;
+    }
+  }
+  return result;
+}
