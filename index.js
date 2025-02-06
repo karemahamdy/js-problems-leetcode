d0978a75b9441d3440424d2713503dc9a0ad84db
Karema <karemahamdy51@gmail.com>
2025-02-06 21:12:14 +0200

    feat : Maximum Ascending Subarray Sum

--- index.js
+++ index.js
@@ -617,3 +617,21 @@ var areAlmostEqual = function (s1, s2) {
     s1[diffs[1]] === s2[diffs[0]];
 };
 
+// Maximum Ascending Subarray Sum
+// Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+// A subarray is defined as a contiguous sequence of numbers in an array.
+//A subarray[numsl, numsl + 1, ..., numsr - 1, numsr] is ascending if for all i where l <= i < r, numsi < numsi + 1. Note that a subarray of size 1 is ascending.
+
+var maxAscendingSum = function (nums) {
+  let maxNum = [0];
+  let currentNum = nums[0]
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      currentNum += nums[i]
+    } else {
+      maxNum = Math.max(maxNum, currentNum)
+      currentNum = nums[i]
+    }
+  }
+  return maxNum = Math.max(maxNum, currentNum)
+};
