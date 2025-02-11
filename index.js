3183aa3f77775ee3cc7f190a6b6d99db80ff1773
Karema <karemahamdy51@gmail.com>
2025-02-11 23:37:23 +0200

    Removing Stars From a String

--- index.js
+++ index.js
@@ -748,4 +748,18 @@ var maxScore = function (s) {
   return ans;
 };
 
+// Removing Stars From a String
 
+var removeStars = function (s) {
+  let arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '*') {
+      arr.splice(arr.length - 1, 1);
+    } else {
+      arr.push(s[i]);
+    }
+  }
+
+  return arr.join('');
+};
