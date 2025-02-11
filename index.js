e44733343b16366f7269661416d451c473e7d0e1
Karema <karemahamdy51@gmail.com>
2025-02-11 23:34:06 +0200

    1726. Tuple with Same Product

--- index.js
+++ index.js
@@ -678,3 +678,35 @@ var queryResults = function (limit, queries) {
 
   return res;
 };
+
+
+// 1726. Tuple with Same Product
+
+// Given an array nums of distinct positive integers, return the number of tuples(a, b, c, d) such that a * b = c * d where a, b, c, and d are elements of nums, and a != b != c != d.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function tupleSameProduct(nums) {
+  let productMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      let product = nums[i] * nums[j];
+      if (productMap.has(product)) {
+        productMap.set(product, productMap.get(product) + 1);
+      } else {
+        productMap.set(product, 1);
+      }
+    }
+  }
+
+  let result = 0;
+  for (let count of productMap.values()) {
+    if (count > 1) {
+
+      result += (count * (count - 1) / 2) * 8;
+    }
+  }
+  return result;
+}
