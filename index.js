2a8fca2974ba942a5137c8726e7ba05b5f52f744
Karema <karemahamdy51@gmail.com>
2025-02-11 23:32:52 +0200

    2130. Maximum Twin Sum of a Linked List

--- index.js
+++ index.js
@@ -635,3 +635,46 @@ var maxAscendingSum = function (nums) {
   }
   return maxNum = Math.max(maxNum, currentNum)
 };
+
+// 3160. Find the Number of Distinct Colors Among the Balls
+
+// You are given an integer limit and a 2D array queries of size n x 2.
+// There are limit + 1 balls with distinct labels in the range[0, limit].Initially, all balls are uncolored.For every query in queries that is of the form[x, y], you mark ball x with the color y.After each query, you need to find the number of distinct colors among the balls.
+// Return an array result of length n, where result[i] denotes the number of distinct colors after ith query.
+// Note that when answering a query, lack of a color will not be considered as a color.
+
+/**
+ * @param {number} limit
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var queryResults = function (limit, queries) {
+  const ballMapColor = new Map();
+  const colorMapCount = new Map();
+  const res = [];
+  let distinct = 0;
+
+  return queries.map(([ball, newColor]) => {
+    const color = ballMapColor.get(ball);
+    ballMapColor.set(ball, newColor);
+    if (color) {
+      colorMapCount.set(color, (colorMapCount.get(color) ?? 0) - 1);
+    }
+
+    if (!colorMapCount.get(newColor)) {
+      distinct++;
+    }
+
+    if (color && !colorMapCount.get(color)) {
+      distinct--;
+    }
+
+    const countNewColor = (colorMapCount.get(newColor) ?? 0)
+
+    colorMapCount.set(newColor, countNewColor + 1);
+
+    return distinct;
+  })
+
+  return res;
+};
