d5cf04702346e95e43ecca31457c2b79a7b1b10c
Karema <karemahamdy51@gmail.com>
2025-02-11 23:35:10 +0200

    1790. Check if One String Swap Can Make Strings Equal

--- index.js
+++ index.js
@@ -710,3 +710,21 @@ function tupleSameProduct(nums) {
   }
   return result;
 }
+
+// 1790. Check if One String Swap Can Make Strings Equal
+
+// You are given two strings s1 and s2 of equal length.A string swap is an operation where you choose two indices in a string(not necessarily different) and swap the characters at these indices.
+// Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings.Otherwise, return false.
+
+var areAlmostEqual = function (s1, s2) {
+  if (s1 === s2) return true;
+
+  let diffs = [];
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] !== s2[i]) diffs.push(i);
+  }
+
+  return diffs.length === 2 &&
+    s1[diffs[0]] === s2[diffs[1]] &&
+    s1[diffs[1]] === s2[diffs[0]];
+};
